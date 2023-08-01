ee6d3abd3a653b23a40b0580a31c32b32955019c
Stanley7096 <865798919@qq.com>
2023-08-01 22:38:49 +0800

    GAN_DDI Model

--- README.md
+++ README.md
@@ -1,14 +1,43 @@
-# SSI-DDI
-Source code for ["SSI–DDI: substructure–substructure interactions for drug–drug interaction prediction"]( https://doi.org/10.1093/bib/bbab133) 
-   
-![Overview of SSI-DDI framework](Framework.png)  
+# Internship project
 
- ## Implementation setup
-The versions are optional, we expect the code to work with latest versions as well.
+### You can find the dataset and related papers on:
+
+https://www.pnas.org/doi/suppl/10.1073/pnas.1803294115
+
+### Deep research on Deep learning improves prediction of drug–drug and drug–food interactions
+
+The **GAN_DDI model** is a deep learning model for predicting drug-drug interactions. It combines the technologies of graph attention network (GAT) and knowledge graph embedding (KGE) to process the structural information of drugs and the interaction relationship between drugs.
+
+The following is a detailed introduction to each part of the GAN_DDI model:
+
+
+* **Graph Attention Network (GAT):** The GAN_DDI model uses a graph attention network to process the structural information of drugs. Specifically, it uses multiple GAT layers, each of which contains multiple attention heads. Each attention head will linearly transform the input node features, and then use the attention mechanism to calculate the interrelationships between nodes, and update the features of the nodes according to these relationships. This enables the model to capture the structural properties of the drug molecule.
+
+
+* **Knowledge Graph Embedding (KGE):** The GAN_DDI model uses knowledge graph embedding to deal with drug interactions. Specifically, it uses the RESCAL model, a knowledge graph embedding model based on matrix factorization. The RESCAL model learns a matrix for each relation, and then uses this matrix to compute the interaction scores for the head and tail nodes. This enables the model to take into account drug-drug interactions.
+
+
+* **Co-attention mechanism:** The GAN_DDI model uses a co-attention mechanism to model the correlation of two drugs. Specifically, it computes attention scores for each pair of drugs across all relationships, and then uses these scores to weight the interaction score for each relationship. This enables the model to better capture drug-drug interactions.
+
+
+* **Training and optimization:** The GAN_DDI model uses SigmoidLoss as the loss function, Adam as the optimizer, and LambdaLR as the learning rate scheduler. SigmoidLoss calculates the logarithmic loss for the prediction scores of positive samples and negative samples separately, and then takes the average as the total loss. The Adam optimizer dynamically adjusts the learning rate of each parameter according to the first-order moment and second-order moment of the gradient. The LambdaLR learning rate scheduler adjusts the learning rate according to a preset function, for example in this model it multiplies the learning rate by 0.96 after each epoch.
+
+
+* **Model structure:** The main body of the GAN_DDI model is a deep neural network, which consists of multiple GAT layers and a KGE layer. Each GAT layer updates the input node features, and then passes the updated features to the next layer. The output features of the last layer will be sent to the KGE layer to calculate the interaction score between drugs.
+
+
+* **Model output**: The output of the GAN_DDI model is the interaction score of each pair of drugs under all relationships. These scores can be converted into interaction probabilities for predicting drug-drug interactions.
+
+
+![GAN_DDI](GAN_DDI.png)  
+
+ ## Requirements
+
 * Python == 3.7
 * [PyTorch](https://pytorch.org/) == 1.6
-* [PyTorch Geometry](https://pytorch-geometric.readthedocs.io/) == 1.6
-* [rdkit](https://www.rdkit.org/) == 2020.09.2
+* [PyTorch Geometry](https://pytorch-geometric.readthedocs.io/) == 2.3.1
+* [rdkit](https://www.rdkit.org/) == 2023.03.2
 
-## Usage
-    python train_script.py <parameters>
+## Environment
+
+You can use Google Colab to run the code. Or you can use your own GPU to run the code.
--- models.py
+++ models.py
@@ -1,6 +1,10 @@
-import torch
-from torch import nn
-import torch.nn.functional as F
+from datetime import datetime
+import time
+import argparse
+from torch import optim
+from sklearn import metrics
+import pandas as pd
+import numpy as np
 from torch.nn.modules.container import ModuleList
 from torch_geometric.nn import (GATConv,
                                 SAGPooling,
@@ -9,14 +13,155 @@ from torch_geometric.nn import (GATConv,
                                 max_pool_neighbor_x,
                                 global_add_pool)
 
-from layers import (
-                    CoAttentionLayer, 
-                    RESCAL, 
-                    RESCAL
-                    )
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+from data_preprocessing import DrugDataset, DrugDataLoader, TOTAL_ATOM_FEATS
+
+######################### Parameters ######################
+parser = argparse.ArgumentParser()
+parser.add_argument('--n_atom_feats', type=int, default=TOTAL_ATOM_FEATS, help='num of input features')
+parser.add_argument('--n_atom_hid', type=int, default=64, help='num of hidden features')
+parser.add_argument('--rel_total', type=int, default=86, help='num of interaction types')
+parser.add_argument('--lr', type=float, default=1e-2, help='learning rate')
+parser.add_argument('--n_epochs', type=int, default=300, help='num of epochs')
+parser.add_argument('--kge_dim', type=int, default=64, help='dimension of interaction matrix')
+parser.add_argument('--batch_size', type=int, default=1024, help='batch size')
+
+parser.add_argument('--weight_decay', type=float, default=5e-4)
+parser.add_argument('--neg_samples', type=int, default=1)
+parser.add_argument('--data_size_ratio', type=int, default=1)
+parser.add_argument('--use_cuda', type=bool, default=True, choices=[0, 1])
+
+args = parser.parse_args()
+n_atom_feats = args.n_atom_feats
+n_atom_hid = args.n_atom_hid
+rel_total = args.rel_total
+lr = args.lr
+n_epochs = args.n_epochs
+kge_dim = args.kge_dim
+batch_size = args.batch_size
+
+weight_decay = args.weight_decay
+neg_samples = args.neg_samples
+data_size_ratio = args.data_size_ratio
+device = 'cuda' if torch.cuda.is_available() and args.use_cuda else 'cpu'
+print(args)
+############################################################
+
+###### Dataset
+df_ddi_train = pd.read_csv('data/ddi_training.csv')
+df_ddi_val = pd.read_csv('data/ddi_validation.csv')
+df_ddi_test = pd.read_csv('data/ddi_test.csv')
+
+train_tup = [(h, t, r) for h, t, r in zip(df_ddi_train['d1'], df_ddi_train['d2'], df_ddi_train['type'])]
+val_tup = [(h, t, r) for h, t, r in zip(df_ddi_val['d1'], df_ddi_val['d2'], df_ddi_val['type'])]
+test_tup = [(h, t, r) for h, t, r in zip(df_ddi_test['d1'], df_ddi_test['d2'], df_ddi_test['type'])]
+
+train_data = DrugDataset(train_tup, ratio=data_size_ratio, neg_ent=neg_samples)
+val_data = DrugDataset(val_tup, ratio=data_size_ratio, disjoint_split=False)
+test_data = DrugDataset(test_tup, disjoint_split=False)
+
+print(f"Training with {len(train_data)} samples, validating with {len(val_data)}, and testing with {len(test_data)}")
+
+train_data_loader = DrugDataLoader(train_data, batch_size=batch_size, shuffle=True)
+val_data_loader = DrugDataLoader(val_data, batch_size=batch_size * 3)
+test_data_loader = DrugDataLoader(test_data, batch_size=batch_size * 3)
 
 
-class SSI_DDI(nn.Module):
+def do_compute(batch, device, training=True):
+    '''
+        *batch: (pos_tri, neg_tri)
+        *pos/neg_tri: (batch_h, batch_t, batch_r)
+    '''
+    probas_pred, ground_truth = [], []
+    pos_tri, neg_tri = batch
+
+    pos_tri = [tensor.to(device=device) for tensor in pos_tri]
+    p_score = model(pos_tri)
+    probas_pred.append(torch.sigmoid(p_score.detach()).cpu())
+    ground_truth.append(np.ones(len(p_score)))
+
+    neg_tri = [tensor.to(device=device) for tensor in neg_tri]
+    n_score = model(neg_tri)
+    probas_pred.append(torch.sigmoid(n_score.detach()).cpu())
+    ground_truth.append(np.zeros(len(n_score)))
+
+    probas_pred = np.concatenate(probas_pred)
+    ground_truth = np.concatenate(ground_truth)
+
+    return p_score, n_score, probas_pred, ground_truth
+
+
+def do_compute_metrics(probas_pred, target):
+    pred = (probas_pred >= 0.5).astype(np.int)
+
+    acc = metrics.accuracy_score(target, pred)
+    auc_roc = metrics.roc_auc_score(target, probas_pred)
+    f1_score = metrics.f1_score(target, pred)
+
+    p, r, t = metrics.precision_recall_curve(target, probas_pred)
+    auc_prc = metrics.auc(r, p)
+
+    return acc, auc_roc, auc_prc
+
+
+class CoAttentionLayer(nn.Module):
+    def __init__(self, n_features):
+        super().__init__()
+        self.n_features = n_features
+        self.w_q = nn.Parameter(torch.zeros(n_features, n_features // 2))
+        self.w_k = nn.Parameter(torch.zeros(n_features, n_features // 2))
+        self.bias = nn.Parameter(torch.zeros(n_features // 2))
+        self.a = nn.Parameter(torch.zeros(n_features // 2))
+
+        nn.init.xavier_uniform_(self.w_q)
+        nn.init.xavier_uniform_(self.w_k)
+        nn.init.xavier_uniform_(self.bias.view(*self.bias.shape, -1))
+        nn.init.xavier_uniform_(self.a.view(*self.a.shape, -1))
+
+    def forward(self, receiver, attendant):
+        keys = receiver @ self.w_k
+        queries = attendant @ self.w_q
+        # values = receiver @ self.w_v
+        values = receiver
+
+        e_activations = queries.unsqueeze(-3) + keys.unsqueeze(-2) + self.bias
+        e_scores = torch.tanh(e_activations) @ self.a
+        # e_scores = e_activations @ self.a
+        attentions = e_scores
+
+        return attentions
+
+
+class RESCAL(nn.Module):
+    def __init__(self, n_rels, n_features):
+        super().__init__()
+        self.n_rels = n_rels
+        self.n_features = n_features
+        self.rel_emb = nn.Embedding(self.n_rels, n_features * n_features)
+        nn.init.xavier_uniform_(self.rel_emb.weight)
+
+    def forward(self, heads, tails, rels, alpha_scores):
+        rels = self.rel_emb(rels)
+        rels = F.normalize(rels, dim=-1)
+        heads = F.normalize(heads, dim=-1)
+        tails = F.normalize(tails, dim=-1)
+        rels = rels.view(-1, self.n_features, self.n_features)
+
+        scores = heads @ rels @ tails.transpose(-2, -1)
+
+        if alpha_scores is not None:
+            scores = alpha_scores * scores
+        scores = scores.sum(dim=(-2, -1))
+        return scores
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.n_rels}, {self.rel_emb.weight.shape})"
+
+
+class GAN_DDI(nn.Module):
     def __init__(self, in_features, hidd_dim, kge_dim, rel_total, heads_out_feat_params, blocks_params):
         super().__init__()
         self.in_features = in_features
@@ -24,17 +169,17 @@ class SSI_DDI(nn.Module):
         self.rel_total = rel_total
         self.kge_dim = kge_dim
         self.n_blocks = len(blocks_params)
-        
+
         self.initial_norm = LayerNorm(self.in_features)
         self.blocks = []
         self.net_norms = ModuleList()
         for i, (head_out_feats, n_heads) in enumerate(zip(heads_out_feat_params, blocks_params)):
-            block = SSI_DDI_Block(n_heads, in_features, head_out_feats, final_out_feats=self.hidd_dim)
+            block = GAN_DDI_Block(n_heads, in_features, head_out_feats, final_out_feats=self.hidd_dim)
             self.add_module(f"block{i}", block)
             self.blocks.append(block)
             self.net_norms.append(LayerNorm(head_out_feats * n_heads))
             in_features = head_out_feats * n_heads
-        
+
         self.co_attention = CoAttentionLayer(self.kge_dim)
         self.KGE = RESCAL(self.rel_total, self.kge_dim)
 
@@ -60,7 +205,7 @@ class SSI_DDI(nn.Module):
 
             h_data.x = F.elu(self.net_norms[i](h_data.x, h_data.batch))
             t_data.x = F.elu(self.net_norms[i](t_data.x, t_data.batch))
-        
+
         repr_h = torch.stack(repr_h, dim=-2)
         repr_t = torch.stack(repr_t, dim=-2)
 
@@ -74,7 +219,7 @@ class SSI_DDI(nn.Module):
         return scores
 
 
-class SSI_DDI_Block(nn.Module):
+class GAN_DDI_Block(nn.Module):
     def __init__(self, n_heads, in_features, head_out_feats, final_out_feats):
         super().__init__()
         self.n_heads = n_heads
@@ -82,11 +227,99 @@ class SSI_DDI_Block(nn.Module):
         self.out_features = head_out_feats
         self.conv = GATConv(in_features, head_out_feats, n_heads)
         self.readout = SAGPooling(n_heads * head_out_feats, min_score=-1)
-    
+
     def forward(self, data):
         data.x = self.conv(data.x, data.edge_index)
-        att_x, att_edge_index, att_edge_attr, att_batch, att_perm, att_scores= self.readout(data.x, data.edge_index, batch=data.batch)
+        att_x, att_edge_index, att_edge_attr, att_batch, att_perm, att_scores = self.readout(data.x, data.edge_index,
+                                                                                             batch=data.batch)
         global_graph_emb = global_add_pool(att_x, att_batch)
 
         # data = max_pool_neighbor_x(data)
         return data, global_graph_emb
+
+
+def train(model, train_data_loader, val_data_loader, loss_fn, optimizer, n_epochs, device, scheduler=None):
+    print('Starting training at', datetime.today())
+    for i in range(1, n_epochs + 1):
+        start = time.time()
+        train_loss = 0
+        train_loss_pos = 0
+        train_loss_neg = 0
+        val_loss = 0
+        val_loss_pos = 0
+        val_loss_neg = 0
+        train_probas_pred = []
+        train_ground_truth = []
+        val_probas_pred = []
+        val_ground_truth = []
+
+        for batch in train_data_loader:
+            model.train()
+            p_score, n_score, probas_pred, ground_truth = do_compute(batch, device)
+            train_probas_pred.append(probas_pred)
+            train_ground_truth.append(ground_truth)
+            loss, loss_p, loss_n = loss_fn(p_score, n_score)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item() * len(p_score)
+        train_loss /= len(train_data)
+
+        with torch.no_grad():
+            train_probas_pred = np.concatenate(train_probas_pred)
+            train_ground_truth = np.concatenate(train_ground_truth)
+
+            train_acc, train_auc_roc, train_auc_prc = do_compute_metrics(train_probas_pred, train_ground_truth)
+
+            for batch in val_data_loader:
+                model.eval()
+                p_score, n_score, probas_pred, ground_truth = do_compute(batch, device)
+                val_probas_pred.append(probas_pred)
+                val_ground_truth.append(ground_truth)
+                loss, loss_p, loss_n = loss_fn(p_score, n_score)
+                val_loss += loss.item() * len(p_score)
+
+            val_loss /= len(val_data)
+            val_probas_pred = np.concatenate(val_probas_pred)
+            val_ground_truth = np.concatenate(val_ground_truth)
+            val_acc, val_auc_roc, val_auc_prc = do_compute_metrics(val_probas_pred, val_ground_truth)
+
+        if scheduler:
+            # print('scheduling')
+            scheduler.step()
+
+        print(f'Epoch: {i} ({time.time() - start:.4f}s), train_loss: {train_loss:.4f}, val_loss: {val_loss:.4f},'
+              f' train_acc: {train_acc:.4f}, val_acc:{val_acc:.4f}')
+        print(
+            f'\t\ttrain_roc: {train_auc_roc:.4f}, val_roc: {val_auc_roc:.4f}, train_auprc: {train_auc_prc:.4f}, val_auprc: {val_auc_prc:.4f}')
+
+
+class SigmoidLoss(nn.Module):
+    def __init__(self, adv_temperature=None):
+        super().__init__()
+        self.adv_temperature = adv_temperature
+
+    def forward(self, p_scores, n_scores):
+        if self.adv_temperature:
+            weights = F.softmax(self.adv_temperature * n_scores, dim=-1).detach()
+            n_scores = weights * n_scores
+        p_loss = - F.logsigmoid(p_scores).mean()
+        n_loss = - F.logsigmoid(-n_scores).mean()
+
+        return (p_loss + n_loss) / 2, p_loss, n_loss
+
+
+model = GAN_DDI(n_atom_feats, n_atom_hid, kge_dim, rel_total, heads_out_feat_params=[32, 32, 32, 32],
+                       blocks_params=[2, 2, 2, 2])
+loss = SigmoidLoss()
+optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+scheduler = optim.lr_scheduler.LambdaLR(optimizer, lambda epoch: 0.96 ** (epoch))
+# print(model)
+model.to(device=device)
+
+# if __name__ == '__main__':
+train(model, train_data_loader, val_data_loader, loss, optimizer, n_epochs, device, scheduler)
+
+
